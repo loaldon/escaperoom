bd9a66a0be4d531fb3999230ab376545b4cf4b76
loaldon <loaldon@ftstudent.org>
2024-12-20 08:53:25 -0500

    END OF WORK DAY

--- script.js
+++ script.js
@@ -9,28 +9,41 @@ $(".diff_button").on("click",diffChange);
 
 function themeChange() {
     let button = $(this);
+    let desc = $("#theme-description");
     $(".theme_button.active").removeClass("active");
     button.addClass("active");
+
     if (button.text() === "Hacker Mission") {
         theme = 1;
+        desc.text("Solve coding puzzles to infiltrate");
+        desc.append("<br> a secured system");
     } else if (button.text() === "Time Traveler") {
         theme = 2;
+        desc.text("Stabilize the timeline by ");
+        desc.append("<br> finding and fixing glitches.");
     } else if (button.text() === "AI Lab Escape") {
         theme = 3;
+        desc.text("The AI system has gone rogue and taken over the lab! ");
+        desc.append("<br> Debug it in order to regain control and escape.");
+
     }
     beginCheck();
 }
 
 function diffChange() {
     let button = $(this);
+    let desc = $("#difficulty-description");
     $(".diff_button.active").removeClass("active");
     button.addClass("active");
     if (button.text() === "Beginner") {
         difficulty = 1;
+        desc.text("Tasks are multiple choice, hints are more detailed, and the time limit is longer.");
     } else if (button.text() === "Intermediate") {
         difficulty = 2;
+        desc.text("Standard time limit, hints, and task difficulty.");
     } else if (button.text() === "Advanced") {
         difficulty = 3;
+        desc.text("Free response tasks, less detailed hints, and shorter time limit.");
     }
     beginCheck();
 }

598003afec9d5db5861c9234b4852746887c1233
loaldon <loaldon@ftstudent.org>
2025-02-03 09:02:06 -0500

    Attempts to add basic functionality to the question page. Doesn't quite work, but it's more functional than before. END OF WORK DAY

--- escape_script.js
+++ escape_script.js
@@ -1,2 +1,32 @@
 $("#difficulty").html("Difficulty: " + difficulty);
 $("#theme").html("Theme: " + theme);
+
+let hints = 3;
+
+let questions =[];
+if (theme === 1) {
+    //add questions to the array i guess
+    console.log(questions);
+}
+
+$("#hint").on('click', {value: hints-1}, updateHints);
+
+function answerQuestion() {
+    event.preventDefault();
+    updateHints(3);
+}
+
+function updateHints(value) {
+    hints = value;
+    let item = $("hints");
+    if (value === 3) {
+        item.text("Hint 1/3");
+    }else if (value === 2) {
+        item.text("Hint 2/3");
+    } else if (value === 1) {
+        item.text("Hint 3/3");
+    } else {
+        item.text("Out of hints!");
+        item.attr("disabled", true);
+    }
+}

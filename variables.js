b5d9b5261b7d5a14d2133a50ba66f45e5d7550a4
loaldon <loaldon@ftstudent.org>
2025-02-11 08:37:48 -0500

    the comments are now before their respective variable definitions. this makes it so that hovering over any mention of the theme and difficulty variables in the IDE will show what each value means.

--- variables.js
+++ variables.js
@@ -1,7 +1,7 @@
-let difficulty = 0;
 //0 for none, 1 for beginner, 2 for intermediate, 3 for advanced
-let theme = 0;
+let difficulty = 0;
 //0 for none, 1 for hacker, 2 for time traveler, 3 for AI lab
+let theme = 0;
 
 //read values from local storage
 if (localStorage.getItem("theme")) {
